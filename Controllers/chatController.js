f233823041a599287d32aebdf289995b5f52c2ec
Abin <abinpv95@gmail.com>
2023-12-14 21:50:02 +0530

    22

--- Controllers/chatController.js
+++ Controllers/chatController.js
@@ -16,6 +16,8 @@ const doctorData = async (req,res) => {
       return res.status(500).json({ message: "Internal Server Error" });
     }
   }
+
+  
 const userData = async (req,res) => {
     try {
       const {id} = req.params
@@ -30,8 +32,44 @@ const userData = async (req,res) => {
 
 const userChats = async(req,res)=>{
     try{
-
-        
+      const {id} = req.params
+      const userId = id
+      const chats = await Chat.aggregate([
+        {
+          $match: { members: userId },
+        },
+        {
+          $lookup: {
+            from: 'messages', // Replace with the actual name of your messages collection
+            let: { chatIdToString: { $toString: '$_id' } }, // Convert _id to string
+            pipeline: [
+              {
+                $match: {
+                  $expr: { $eq: ["$chatId", "$$chatIdToString"] }, // Match on the converted chatId
+                },
+              },
+              {
+                $sort: { createdAt: -1 }, // Sort messages in descending order based on timestamp
+              },
+              {
+                $limit: 1, // Get only the latest message
+              },
+            ],
+            as: 'messages',
+          },
+        },
+        {
+          $addFields: {
+            lastMessageTimestamp: {
+              $ifNull: [{ $first: '$messages.createdAt' }, null],
+            },
+          },
+        },
+        {
+          $sort: { lastMessageTimestamp: -1 }, // Sort chats based on the latest message timestamp
+        },
+      ]);
+      res.status(200).json(chats); 
     }catch(error){
         console.log(error.message)
         res.status(500),json({message:"Internal error"})
--- Routes/chatRoutes.js
+++ Routes/chatRoutes.js
@@ -4,6 +4,6 @@ const chatController = require('../Controllers/chatController')
 
 chatRoute.get('/doctorData/:id',chatController.doctorData)
 chatRoute.get('/userData/:id',chatController.userData)
-chatRoute.get("/:userId",chatController.userChats)
+chatRoute.get("/chat/:id",chatController.userChats)
 
 module.exports = chatRoute
